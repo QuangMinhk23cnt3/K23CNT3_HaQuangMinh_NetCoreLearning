6377d38a35c37084d807e44d1ea4abdd59fa821d
Minh Quang <haquangminhk23cnt3@gmail.com>
2025-04-29 19:49:22 +0700

    hqmlesson02

---
+++ HqmLesson02/Controllers/HqmProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HqmLesson02.Controllers
+{
+    public class HqmProductController : Controller
+    {
+        public IActionResult HqmIndex()
+        {
+            ViewData["messageData"] = "Du lieu tu viewData";
+            ViewBag.messageData = "Du lieu tu ViewBag";
+            TempData["messageData"] = "Du lieu TempData";
+            return View();
+        }
+    }
+}
