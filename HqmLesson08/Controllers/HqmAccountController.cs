3cbfcd9285a820f7df7afaebe0796460ea929153
Minh Quang <haquangminhk23cnt3@gmail.com>
2025-06-10 11:28:28 +0700

    hqmLesson08Annotation

---
+++ HqmLesson08/Controllers/HqmAccountController.cs
@@ -0,0 +1,166 @@
+﻿using HqmLesson08.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HqmLesson08.Controllers
+{
+    public class HqmAccountController : Controller
+    {
+        private static List<HqmAccount> hqmListAccount = new List<HqmAccount>()
+        {
+            new HqmAccount
+            {
+                HqmId = 231090067,
+                HqmFullName = "Ha Quang Minh",
+                HqmEmail = "haquangminhk23cnt3@gmail.com",
+                HqmPhone = "0846409694",
+                HqmAddress = "Hà Nội",
+                HqmAvatar = "avatar1.jpg",
+                HqmBirthday = new DateTime(2005, 6, 2),
+                HqmGender = "Nam",
+                HqmPassword = "123456@",
+                HqmFacebook = "https://www.facebook.com/ha.q.minh206/"
+            },
+            new HqmAccount
+            {
+                HqmId = 2,
+                HqmFullName = "Trần Thị B",
+                HqmEmail = "tranthib@example.com",
+                HqmPhone = "0909876543",
+                HqmAddress = "Đà Nẵng",
+                HqmAvatar = "avatar2.jpg",
+                HqmBirthday = new DateTime(1998, 10, 1),
+                HqmGender = "Nữ",
+                HqmPassword = "abcdef",
+                HqmFacebook = "https://facebook.com/tranthib"
+            },
+            new HqmAccount
+            {
+                HqmId = 3,
+                HqmFullName = "Lê Văn C",
+                HqmEmail = "levanc@example.com",
+                HqmPhone = "0938123456",
+                HqmAddress = "TP.HCM",
+                HqmAvatar = "avatar3.jpg",
+                HqmBirthday = new DateTime(1990, 3, 20),
+                HqmGender = "Nam",
+                HqmPassword = "pass123",
+                HqmFacebook = "https://facebook.com/levanc"
+            },
+            new HqmAccount
+            {
+                HqmId = 4,
+                HqmFullName = "Phạm Thị D",
+                HqmEmail = "phamthid@example.com",
+                HqmPhone = "0987654321",
+                HqmAddress = "Cần Thơ",
+                HqmAvatar = "avatar4.jpg",
+                HqmBirthday = new DateTime(2000, 7, 15),
+                HqmGender = "Nữ",
+                HqmPassword = "mypass",
+                HqmFacebook = "https://facebook.com/phamthid"
+            },
+            new HqmAccount
+            {
+                HqmId = 5,
+                HqmFullName = "Đỗ Mạnh E",
+                HqmEmail = "domanhe@example.com",
+                HqmPhone = "0977777777",
+                HqmAddress = "Hải Phòng",
+                HqmAvatar = "avatar5.jpg",
+                HqmBirthday = new DateTime(1992, 12, 25),
+                HqmGender = "Nam",
+                HqmPassword = "securepw",
+                HqmFacebook = "https://facebook.com/domanhe"
+            }
+        };
+        // GET: HqmAccountController
+        public ActionResult HqmIndex()
+        {
+            return View(hqmListAccount);
+        }
+
+        // GET: HqmAccountController/Details/5
+        public ActionResult HqmDetails(int id)
+        {
+            var hqmAccount = hqmListAccount.FirstOrDefault(x => x.HqmId == id);
+            return View(hqmAccount);
+        }
+
+        // GET: HqmAccountController/Create
+        public ActionResult HqmCreate()
+        {
+            var hqmModel = new HqmAccount();
+            return View(hqmModel);
+        }
+
+        // POST: HqmAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HqmCreate(HqmAccount hqmModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    hqmListAccount.Add(hqmModel);
+
+                    return RedirectToAction(nameof(HqmIndex));
+                }
+
+                // Nếu dữ liệu không hợp lệ, trả lại View với dữ liệu cũ
+                return View(hqmModel);
+            }
+            catch (Exception ex)
+            {
+                // Ghi log nếu cần
+                ModelState.AddModelError("", "Có lỗi xảy ra khi thêm mới tài khoản." + ex.Message);
+                return View(hqmModel);
+            }
+        }
+
+        // GET: HqmAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HqmAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HqmAccountController/Delete/5
+        public ActionResult HqmDelete(int id)
+        {
+            var hqmAccount = hqmListAccount.FirstOrDefault(e => e.HqmId == id);
+            if (hqmAccount != null) hqmListAccount.Remove(hqmAccount);
+            return RedirectToAction("HqmIndex");
+        }
+
+        // POST: HqmAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HqmLesson08/Models/HqmAccount.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HqmLesson08.Models
+{
+    public class HqmAccount
+    {
+        [Key]
+
+        [Display(Name = "Ma")]
+        public int HqmId { get; set; }
+
+        [Display(Name ="Ho va ten")]
+        [Required(ErrorMessage ="Ho va ten ko duoc de trong!!!")]
+        [MinLength(6, ErrorMessage ="Ho ten phai it nhat 6 ky tu!!!")]
+        [MaxLength(20, ErrorMessage ="Ho ten chi dc toi da 20 ky tu!!!")]
+        public string HqmFullName { get; set; }
+
+        [Display(Name = "Địa chỉ email")]
+        [Required(ErrorMessage = "Địa chỉ email không được để trống")]
+        [DataType(DataType.EmailAddress)]
+        public string HqmEmail { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\+?\d{1,3})?[-. ]?(\(?\d{3}\)?)[-. ]?\d{3}[-. ]?\d{4}$",
+            ErrorMessage = "Số điện thoại không đúng định dạng")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        public string HqmPhone { get; set; }
+
+        [Display(Name = "Địa chỉ thường trú")]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        [StringLength(35, ErrorMessage = "Địa chỉ không vượt quá 35 ký tự")]
+        public string HqmAddress { get; set; }
+
+        [Display(Name = "Ảnh đại diện")]
+        public string HqmAvatar { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        [DataType(DataType.Date)]
+        public DateTime HqmBirthday { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public string HqmGender { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage ="Mat khau ko dc de trong")]
+        [MinLength(6, ErrorMessage ="Mat khau it nhat 6 ki tu")]
+        public string HqmPassword { get; set; }
+
+        [Display(Name = "Facebook")]
+        public string HqmFacebook { get; set; }
+    }
+}
